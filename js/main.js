8c200271952a8af66b21f209b0c42ff60fb5e8a6
lfrestrepo004sena <70179712+lfrestrepo004sena@users.noreply.github.com>
2022-04-01 15:36:27 -0500

    Commit initial

---
+++ js/main.js
@@ -0,0 +1,13 @@
+function suma(){
+var a;
+var b;
+var resultado;
+
+a=parseInt(prompt("POR FAVOR INGRESE EL PRIMER VALOR:"));
+b=parseInt(prompt("POR FAVOR INGRESE EL SEGUNDO VALOR:"));
+
+resultado = a + b;
+
+alert("El resultado de la suma es: " +
+     resultado);
+}
